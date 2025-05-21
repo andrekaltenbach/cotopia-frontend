77122e87798848af53e8af12a5e533a7fd2328e2
Andre Kaltenbach <andre.kaltenbach@posteo.de>
2025-05-21 14:34:53 +0200

    feat: add footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <div className="Footer p-5 border-t-1">
+      <p className="text-center text-sm text-gray-500">Cotopia 2025. All rights reserved</p>
+    </div>
+  );
+}
+
+export default Footer;
